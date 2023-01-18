18f00bb6f4db3a606dc891dd2624ad5f9749868d
DVidal1209 <daniel.vidal17@yahoo.com>
2023-01-18 16:09:47 -0500

    Completed Project component

--- my-portfolio/src/components/Project.js
+++ my-portfolio/src/components/Project.js
@@ -12,32 +12,74 @@ function Project() {
 
     const styles = {
         img: {
-            width: '300px',
+            width: '100%',
+            border: '0.5%'
         },
         imgWrap: {
             position: 'relative',
-            maxWidth: '30%'
+            // maxWidth: '30%'
         },
         imgDescription: {
             position: 'absolute',
-            top: '35%',
+            height: '100%',
+            paddingTop: '30%',
+            paddingLeft: '2%',
+            top: '0',
             bottom: '0',
-            left: '10%',
+            left: '0',
             right: '0',
-            background: 'rgba(0, 0, 0, 0.2)',
-            color: '#fff',
-            visibility: isHover ? 'visible' :  'hidden',
-            opacity: isHover ? '1' : '0'
+            maxWidth: '100%',
+            background: 'rgba(0, 0, 0, 0.1)',
+            color: '#093',
+            visibility: isHover ? 'visible' : 'hidden',
+            opacity: isHover ? '1' : '0',
+            fontSize: '125%',
+            fontWeight: 'bold'
+        },
+        linkStyle: {
+            textDecoration: 'none'
         }
     }
 
     return (
-        <div style={styles.imgWrap} 
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        class = "container">
-            <img style = { styles.img } src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674069290/IMG_1005_kwc7iq.jpg" />
-            <p style={styles.imgDescription}>This image looks super neat.</p>
+        <div class="container" style={{ display: 'flex' }}>
+            <div style={styles.imgWrap}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                class="col-5">
+                <img style={styles.img} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674074536/Aesthetic_20Search_20Engine_nsmzls.png" alt = "Aesthetic Search Engine Screenshot" />
+                <div style={styles.imgDescription}>
+                    <p>
+                        <a style = {styles.linkStyle} href="https://dvidal1209.github.io/project-1/">
+                            Aesthetic Search Engine
+                        </a>
+                        <a href="https://github.com/DVidal1209/project-1">
+                            <img style={{ height: 25 }} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674074894/github-mark_1_xtdjwg.png" alt = "GitHub Logo" />
+                        </a>
+                        <br></br>
+                        Javascript/HTML/CSS
+                    </p>
+                </div>
+            </div>
+            <div class='col-2'></div>
+            <div style={styles.imgWrap}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                class="col-5">
+                <img style={styles.img} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674075936/06-server-side-apis-homework-demo_xmkygh.png" />
+                <div style={styles.imgDescription}>
+                    <p>
+                        <a style = {styles.linkStyle} href="https://dvidal1209.github.io/weather-call/">
+                            Weather Forecast
+                        </a>
+                        <a href="https://github.com/DVidal1209/weather-call">
+                            <img style={{ height: 25 }} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674074894/github-mark_1_xtdjwg.png" alt = "GitHub Logo" />
+                        </a>
+                        <br></br>
+                        Javascript/Accuweather API/HTML
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
