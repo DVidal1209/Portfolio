85beba185862cb47c44fc2e8f86fcde5dad84727
DVidal1209 <daniel.vidal17@yahoo.com>
2023-01-18 19:17:30 -0500

    Created Footer Component

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react"
 
-function Footer () {
-    
-    return(
-        <div style = {{ display: 'flex', position: 'relative', bottom: 0, height: 100, backgroundColor: '#62929E' }}>
-            
+function Footer() {
+
+    return (
+        <div style={{ textAlign: "center", position: 'relative', bottom: 0, height: 100, backgroundColor: '#62929E', alignItems: 'center', display: 'flex' }}>
+            <div class='container'>
+            <a href="https://github.com/DVidal1209" style = {{ padding: 5 }}>
+                <img style={{ height: 25 }} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674084018/github-mark-white_gpcgni.png" alt="GitHub Logo" />
+            </a>
+            <a href = "" style = {{ padding: 5 }}>
+            <img style={{ height: 25 }} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674086590/LI-In-Bug_ayezvw.png" alt="LinkedIn Logo" /> 
+            </a>
+            </div>
         </div>
     )
 }
