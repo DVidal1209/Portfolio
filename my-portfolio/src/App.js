244ec604c6fd4dd5590936102fa75a67f58e370f
DVidal1209 <daniel.vidal17@yahoo.com>
2023-02-22 14:13:33 -0500

    Updated title

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,10 +3,18 @@ import React from "react";
 import './App.css';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
+import { useEffect } from 'react';
+
+function Title() {
+  useEffect(() => {
+    document.title = 'Daniel Vidal Portfolio';
+  }, []);
+}
 
 function App() {
   return (
-    <main style= {{ backgroundColor: '#E9EBF8', minHeight: "100%" }}>
+    <main style= {{ backgroundColor: '#E9EBF8', minHeight: "100%" }}> 
+      <Title />
       <Portfolio />
       <Footer />
     </main>
