173de41b3477d2442a2f7473b59ded64698599d1
DVidal1209 <daniel.vidal17@yahoo.com>
2023-03-29 17:12:03 -0400

    Edited About Me

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -9,7 +9,7 @@ function About() {
                 <img style={{ height: 200, borderRadius: '50%' }} src="https://res.cloudinary.com/dfe0rjexj/image/upload/v1674069290/IMG_1005_kwc7iq.jpg" id="daniel-vidal-image" />
                 {"\n\n"}
                 <p>
-                I am a Back End Developer who recently earned a certificate in Full Stack Development from Case Western University, where I developed skills in Node.js, React.js, MongoDB and more.
+                I am a Full Stack Developer who recently earned a certificate in Full Stack Development from Case Western University, where I developed skills in Node.js, React.js, MongoDB and more.
                 {"\n\n"}
                 An innovative problem-solver passionate about developing apps. I have continued my studies independently to learn more about data streaming to the client
                 {"\n\n"}
